205e0eceb08ea4826b2f318232ec37e311bb7e3d
romy-bos <r.bos28@ncf.edu>
2026-08-20 01:04:39 +0000

    Finish code tutorial

--- script-2.R
+++ script-2.R
@@ -1,3 +1,27 @@
 library(tidyverse)
 
 # Had an OK filet mignon at Cheesecake Factory.
+
+diamonds_histogram <- ggplot(diamonds, aes(x = carat)) +
+	geom_histogram(binwidth = 0.05, fill = "gray40", color = "white") +
+	labs(
+		title = "Histogram of Carat (Weight) among 50,000 Diamonds",
+		subtitle = "Carats just at or above meaningful numbers are very common",
+		x = "Carat",
+		y = "Number",
+		caption = "diamonds dataset from ggplot2 package"
+	) +
+	theme_gray(base_size = 18) +
+	xlim(0, 5.2)
+
+ggsave(
+	"diamonds-histogram.png",
+	plot = diamonds_histogram,
+	width = 12.24,
+	height = 6.76,
+	units = "in",
+	dpi = 100
+)
+
+diamonds_histogram
+
